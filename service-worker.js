087fcd3ce31ddecdf8cf79d98b7ee7238d559526
lars-coding-oh <lame.rodriguez.ui@phinmaed.com>
2026-03-01 07:33:12 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open('todo-cache').then(function(cache) {
-            return cache.addAll([index.html]);
-        })
-    );
+self.addEventListener("install", function(e){
+  e.waitUntil("todo-cache".then(function(cache){
+    return cache.addAll(["index.html"]);
+  })
+);
 });
